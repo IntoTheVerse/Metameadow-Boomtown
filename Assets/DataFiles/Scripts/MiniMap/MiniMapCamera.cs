172138e3af499e08646f4442bb05198b37d4b3ad
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2024-05-11 16:10:56 +0530

    Minimap pointer bug

--- Assets/DataFiles/Scripts/MiniMap/MiniMapCamera.cs
+++ Assets/DataFiles/Scripts/MiniMap/MiniMapCamera.cs
@@ -8,20 +8,29 @@ public class MiniMapCamera : MonoBehaviour
            public float height = 100;
            public bool followPosition; //should the camera rotate with the target
            public bool followRotation; //should the camera rotate with the target
-   
-/*           void Update()
-           {
-               try
-               {
-                   if (!target)
-                       target = FindObjectOfType<vThirdPersonController>().transform ;
-               }
-               catch (Exception e)
-               {
-               }
-           }*/
-   
-           void LateUpdate()
+
+    void Update()
+    {
+        try
+        {
+            if (!target )
+            {
+                var players = FindObjectsOfType<vThirdPersonController>();
+                foreach ( vThirdPersonController p in players)
+                {
+                    if (p.photonView.IsMine)
+                    {
+                        target = FindObjectOfType<vThirdPersonController>().transform;
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+        }
+    }
+
+    void LateUpdate()
            {
                if (!target) return;
    
